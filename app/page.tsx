60ce33ae58842328f7cae7fbe58cb73748454c78
WarFiN <rajvirgoenka@gmail.com>
2024-10-23 00:14:41 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "GitHub", href: "https://github.com/warfin123" },
+  { name: "GitHub", href: "https://github.com/warfin123", target="_blank" },
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
 ];
